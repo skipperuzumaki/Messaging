f599d21e45e8e22ea1c055ba713a9e1b79c1d92c
MD Ruhulamin Khan <skipperuzumaki@gmail.com>
2022-02-18 17:04:41 +0530

    completed CRUD operations on user

--- db/queries/user.sql
+++ db/queries/user.sql
@@ -4,3 +4,22 @@ INSERT INTO "users"(
 ) VALUES (
     $1
 ) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM "users"
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM "users"
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE "users"
+set username = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM "users" WHERE id = $1;
